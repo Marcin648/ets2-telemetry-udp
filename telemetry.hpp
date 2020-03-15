4a8bcea70b3628f8164572035dc247ec807c24bb
Marcin648 <marcin648@gmail.com>
2020-03-15 15:30:20 +0100

    Add trailer telemetry

--- telemetry.hpp
+++ telemetry.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include <cstdint>
 
 struct __attribute__((packed)) telemetry_fvector_t {
@@ -7,15 +8,17 @@ struct __attribute__((packed)) telemetry_fvector_t {
 };
 
 struct __attribute__((packed)) telemetry_euler_t {
-    scs_float_t heading;
-    scs_float_t pitch;
-    scs_float_t roll;
+    float heading;
+    float pitch;
+    float roll;
 };
 
 struct __attribute__((packed)) telemetry_fplacement_t {
     telemetry_fvector_t position;
     telemetry_euler_t orientation;
 };
+
+
 
 struct telemetry_truck_s {
     telemetry_fplacement_t world_placement;
@@ -41,43 +44,43 @@ struct telemetry_truck_s {
     float effective_clutch;
     float cruise_control;
     uint32_t hshifter_slot;
-    bool hshifter_selector[4]; // TODO index??
-    bool parking_brake;
-    bool motor_brake;
+    uint8_t hshifter_selector[4]; // TODO index??
+    uint8_t parking_brake;
+    uint8_t motor_brake;
     uint32_t retarder_level;
     float brake_air_pressure;
-    bool brake_air_pressure_warning;
-    bool brake_air_pressure_emergency;
+    uint8_t brake_air_pressure_warning;
+    uint8_t brake_air_pressure_emergency;
     float brake_temperature;
     float fuel;
-    bool fuel_warning;
+    uint8_t fuel_warning;
     float fuel_average_consumption;
     float fuel_range;
     float adblue;
-    bool adblue_warning;
+    uint8_t adblue_warning;
     float adblue_average_consumption;
     float oil_pressure;
-    bool oil_pressure_warning;
+    uint8_t oil_pressure_warning;
     float oil_temperature;
     float water_temperature;
-    bool water_temperature_warning;
+    uint8_t water_temperature_warning;
     float battery_voltage;
-    bool battery_voltage_warning;
-    bool electric_enabled;
-    bool engine_enabled;
-    bool lblinker;
-    bool rblinker;
-    bool light_lblinker;
-    bool light_rblinker;
-    bool light_parking;
-    bool light_low_beam;
-    bool light_high_beam;
+    uint8_t battery_voltage_warning;
+    uint8_t electric_enabled;
+    uint8_t engine_enabled;
+    uint8_t lblinker;
+    uint8_t rblinker;
+    uint8_t light_lblinker;
+    uint8_t light_rblinker;
+    uint8_t light_parking;
+    uint8_t light_low_beam;
+    uint8_t light_high_beam;
     uint32_t light_aux_front;
     uint32_t light_aux_roof;
-    bool light_beacon;
-    bool light_brake;
-    bool light_reverse;
-    bool wipers;
+    uint8_t light_beacon;
+    uint8_t light_brake;
+    uint8_t light_reverse;
+    uint8_t wipers;
     float dashboard_backlight;
     float wear_engine;
     float wear_transmission;
@@ -89,7 +92,7 @@ struct telemetry_truck_s {
     float navigation_time;
     float navigation_speed_limit;
     float wheel_susp_deflection[4]; // TODO index??
-    bool wheel_on_ground[4]; // TODO index??
+    uint8_t wheel_on_ground[4]; // TODO index??
     uint32_t wheel_substance[4]; // TODO index??
     float wheel_velocity[4]; // TODO index??
     float wheel_steering[4]; // TODO index??
@@ -98,31 +101,21 @@ struct telemetry_truck_s {
     float wheel_lift_offset[4]; // TODO index??
 };
 
-/*
-struct __attribute__((packed)) telemetry_truck_s {
-    uint32_t telemetry_version;
-    uint8_t game_paused; // 0 - false; 1 - true
-    int32_t game_time; // in minute
-    int32_t next_rest_stop; // in minute
 
-    uint8_t truck_engine_enabled; // 0 - false; 1 - true
-    uint8_t truck_light_parking; // 0 - false; 1 - true
-    uint8_t truck_light_low_beam; // 0 - false; 1 - true
-    uint8_t truck_light_high_beam; // 0 - false; 1 - true
-    uint32_t truck_light_aux_front; // 1 - dimmed state; 2 - full state
-    uint32_t truck_light_aux_roof; // 1 - dimmed state; 2 - full state
+struct telemetry_trailer_s{
+    uint8_t connected;
+    telemetry_fplacement_t world_placement;
+    telemetry_fvector_t local_linear_velocity;
+    telemetry_fvector_t local_angular_velocity;
+    telemetry_fvector_t local_linear_acceleration;
+    telemetry_fvector_t local_angular_acceleration;
 
-    uint8_t truck_parking_brake; // 0 - false; 1 - true
-    uint8_t truck_lblinker; // 0 - false; 1 - true
-    uint8_t truck_rblinker; // 0 - false; 1 - true
-    uint8_t truck_light_lblinker; // 0 - false; 1 - true
-    uint8_t truck_light_rblinker; // 0 - false; 1 - true
+    float wear_chassis;
 
-    float truck_engine_rpm;
-    float truck_speed; 
-    float truck_fuel;
-
-	//scs_value_dplacement_t world_placement;
-
+    float wheel_susp_deflection[4]; // TODO index??
+    uint8_t wheel_on_ground[4]; // TODO index??
+    uint32_t wheel_substance[4]; // TODO index??
+    float wheel_velocity[4]; // TODO index??
+    float wheel_steering[4]; // TODO index??
+    float wheel_rotation[4]; // TODO index??
 };
-*/
