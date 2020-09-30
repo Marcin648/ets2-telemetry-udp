53d8eddc065d0ccca8436ad728992ca2b1cf0565
Marcin648 <marcin648@gmail.com>
2020-09-30 19:31:28 +0200

    Version v0.2.0

--- include/defines.hpp
+++ include/defines.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
-#define VERSION "v0.1.0"
+#define VERSION "v0.2.0"
 #define TELEMETRY_VERSION 0
 #define LOG_PREFIX "[TELE-UDP] "
 #define BIND_PORT 23444
 #define CLIENT_TIMEOUT 5.0
+
+#define SEND_RATE_MAX 30.0
+#define SEND_RATE_CONFIG 1.0
